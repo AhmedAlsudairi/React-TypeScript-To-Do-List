63eb1a2b3043c672ef3a81828c81c4ce71f405b9
Ahmed Alsudairi <a7madalsudairi@gmail.com>
2021-06-01 13:56:37 +0300

    creating text field component

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { TextField } from './components/textField';
 function App() {
   return (
     <div className="App">
+      <br></br>
+      <h1>To Do List</h1>
       <TextField text="Ahmed" />
     </div>
   );
--- src/components/textField.tsx
+++ src/components/textField.tsx
@@ -1,12 +1,16 @@
-import React, { useState } from 'react';
+import React, { ChangeEventHandler, useState } from 'react';
 
 interface Props{
     text?: string
 }
+
 export const TextField: React.FC<Props> = (props) => {
     const [text, setText] = useState<string | undefined>('')
 
+    const handleChange = (event: React.ChangeEvent, value: string) => {
+        setText(value);
+    }
     return(
-        <input type='text'></input>
+        <input type='text' value={text} onChange={(event) => handleChange(event,event.target.value)}></input>
     )
 }
