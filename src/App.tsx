10b68e1b056b3d6cd79d94504ffc98763d12a1f3
Ahmed Alsudairi <a7madalsudairi@gmail.com>
2021-06-01 15:20:02 +0300

    adding chakra ui library

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import { TextField } from './components/textField';
-
+import { SubmitBtn } from './components/submitBtn';
 function App() {
+  const [items, setItems] = useState<string []>([])
+  const [text, setText] = useState<string>('')
+
+  const handleChange = (value: string) => {
+    console.log(value);
+    
+      setText(value);
+  }
+
+  const submitItem = () => {
+    setText('');
+    setItems([...items, text]);
+}
   return (
     <div className="App">
       <br></br>
       <h1>To Do List</h1>
-      <TextField text="Ahmed" />
+      <TextField text={text} handleChange={(text)=>{handleChange(text)}}/>
+      <br></br>
+      <SubmitBtn handleClick={submitItem}/>
+      <br></br>
+      <div>
+        {items.map((item,index)=>
+          <h6 key={index}>{item}</h6>
+        )}
+      </div>
     </div>
   );
 }
--- src/components/textField.tsx
+++ src/components/textField.tsx
@@ -1,16 +1,16 @@
-import React, { ChangeEventHandler, useState } from 'react';
+import React from 'react';
+import { Input } from "@chakra-ui/react"
 
 interface Props{
-    text?: string
+    text?: string,
+    handleChange: (text: string) => void
 }
 
 export const TextField: React.FC<Props> = (props) => {
-    const [text, setText] = useState<string | undefined>('')
 
-    const handleChange = (event: React.ChangeEvent, value: string) => {
-        setText(value);
-    }
     return(
-        <input type='text' value={text} onChange={(event) => handleChange(event,event.target.value)}></input>
+
+    <Input placeholder="Basic usage" value={props.text} onChange={(event) => props.handleChange(event.target.value)} size="lg"/>
+        
     )
 }
